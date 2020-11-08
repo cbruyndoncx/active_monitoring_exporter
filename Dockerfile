74ee2ee30e9fcf051c2212887a588e4033862b96
Rory Shanks <megalan247@users.noreply.github.com>
2020-11-08 18:20:28 +0100

    Added runtime metrics

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:alpine
 ENV CHROME_BIN="/usr/bin/chromium-browser" \
     PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
 WORKDIR /app
-RUN apk add --no-cache bash udev ttf-freefont chromium
+RUN apk add --no-cache bash udev ttf-freefont chromium coreutils
 COPY package.json /app
 RUN npm i
 COPY codecept.conf.js /app/
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,18 +5,25 @@ while true; do
     TESTS=$(ls -l checks/ | awk '{print $9}' | sed 's/.js//')
     echo '# HELP active_monitoring_success Active test success by test and target' > tmp
     echo '# TYPE active_monitoring_success gauge' >> tmp
+    echo '# HELP active_monitoring_runtime Active test runtime by test and target' > tmp.runtime
+    echo '# TYPE active_monitoring_runtime gauge' >> tmp.runtime
     for TEST in $TESTS; do
         TARGETS=$(cat targets/$TEST)
         for TARGET in $TARGETS; do
             export BASE_URL=$TARGET
+            START=`date +%s%3N`
             npx codeceptjs run checks/${TEST}.js &> monitoring.log
+            END=`date +%s%3N`
+            RUNTIME=$((END-START))
             if [ $? -eq 0 ]; then
                 echo "active_monitoring_success{test=\"$TEST\",target=\"$TARGET\"} 1" >> tmp
             else
                 echo "active_monitoring_success{test=\"$TEST\",target=\"$TARGET\"} 0" >> tmp
             fi
+            echo "active_monitoring_runtime{test=\"$TEST\",target=\"$TARGET\"} $RUNTIME" >> tmp.runtime
         done
     done
     mv tmp result.txt
+    cat tmp.runtime >> result.txt
     sleep 10
 done
